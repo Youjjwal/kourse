44e1cedf5cabe1be48de458ed7a699f30f39f835
Ujjwal kashyap <156573475+Youjjwal@users.noreply.github.com>
2025-04-28 23:44:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 api_id = "29334124"
 api_hash = "46eea567fd25be2efbf82af17eefea93"
-bot_token = "7858736225:AAGxMwlXFg9NxmIV3w0t0zqzIMZCzIhT1Tc"
+bot_token = "7858736225:AAGkCkiMpcqns4jPMOn3HMm6xeX4Eh9dEBo"
 auth_users = [5344733200]
 sudo_users = [5344733200]
