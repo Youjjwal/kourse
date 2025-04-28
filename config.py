18acb7765c7d7c977b5b97758e6bbd7269e09297
Ujjwal kashyap <156573475+Youjjwal@users.noreply.github.com>
2025-04-28 23:35:02 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+api_id = "29334124"
+api_hash = "46eea567fd25be2efbf82af17eefea93"
+bot_token = "7858736225:AAGxMwlXFg9NxmIV3w0t0zqzIMZCzIhT1Tc"
+auth_users = [5344733200]
+sudo_users = [5344733200]
